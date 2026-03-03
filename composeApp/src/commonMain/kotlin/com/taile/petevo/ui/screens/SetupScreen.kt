ca1bfd78bc6e635d5823d35650408ed21007293f
TaiLe <lephuoctai2006@gmail.com>
2026-03-03 10:27:35 +0700

    feat: update SetupScreen layout to center content vertically

--- composeApp/src/commonMain/kotlin/com/taile/petevo/ui/screens/SetupScreen.kt
+++ composeApp/src/commonMain/kotlin/com/taile/petevo/ui/screens/SetupScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.composables.icons.fontawesome.FontAwesome
@@ -247,7 +248,8 @@ private fun ModeCard(
     ) {
         Column(
             modifier = Modifier.padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
             Image(
                 icon,
